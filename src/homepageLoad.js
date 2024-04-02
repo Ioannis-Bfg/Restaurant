1b3be7465a773308a93349f5c1eac4590bca30e7
Ioannis Boufogiannis <ioannisboufogiannis@gmail.com>
2024-04-02 19:50:16 +0300

    made the functionality of the website switch pages

---
+++ src/homepageLoad.js
@@ -0,0 +1,25 @@
+export function HomepageLoad() {
+    const h1 = document.createElement('h1');
+    const img = document.createElement('img');
+    const p = document.createElement('p');
+    const content = document.querySelector('#content');
+    const container=document.createElement('div');
+    const p_cont=document.createElement('div');
+    const hero=document.createElement('div');
+
+    hero.setAttribute('id','hero');
+    p_cont.setAttribute('id','pcontainer')
+    container.setAttribute('id','homepagecontainer')
+    h1.textContent = 'TZATZIKI';
+    img.src = "../images/restaurant.jpg";
+    p.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis quibusdam quam doloremque consequatur nemo, sed facere eveniet Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis quibusdam quam doloremque consequatur nemo, sed facere eveniet Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis quibusdam quam doloremque consequatur nemo, sed facere eveniet';
+
+    p_cont.appendChild(p);
+    
+    hero.appendChild(img);
+    hero.appendChild(p_cont);
+    
+    container.appendChild(h1);
+    container.appendChild(hero);
+    content.appendChild(container);
+}
